01341b32b81ee4e971e15741cd61d1c9d871e528
KRD <kyle.dyer@gmail.com>
2022-07-20 06:13:37 -0700

    moved close button to container

--- bankey-app/bankey-app/Onboarding/OnboardingContainerViewController.swift
+++ bankey-app/bankey-app/Onboarding/OnboardingContainerViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class OnBoardingContainerViewController:UIViewController {
     
+    let closeButton = UIButton()
+    
     let pageViewController: UIPageViewController
     var pages = [UIViewController]()
     var currentVC: UIViewController {
@@ -19,11 +21,10 @@ class OnBoardingContainerViewController:UIViewController {
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         self.pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
         
-//        let page1 = ViewController1()
-        let page1 = OnboardingViewController()
-        let page2 = ViewController2()
-        let page3 = ViewController3()
-        
+        let page1 = OnboardingViewController(heroImageName: "delorean", titleText: "Bankey is faster, easier to use and has a brand new look and feel that will make you feel like you are back in the 1980s.")
+        let page2 = OnboardingViewController(heroImageName: "world", titleText: "Move your money around the world quickly and securely.  That way the government can't seize your assets once they're on to you.")
+        let page3 = OnboardingViewController(heroImageName: "thumbs", titleText: "For details go to www.bankey.com. \n\n Now let's get started!")
+
         pages.append(page1)
         pages.append(page2)
         pages.append(page3)
@@ -40,12 +41,94 @@ class OnBoardingContainerViewController:UIViewController {
     override func viewDidLoad() {
         
         view.backgroundColor = .systemPurple
-        
+
         addChild(pageViewController)
         view.addSubview(pageViewController.view)
+        view.addSubview(closeButton)
+
         pageViewController.didMove(toParent: self)
-        
+
         pageViewController.dataSource = self
+        pageViewController.view.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            view.topAnchor.constraint(equalTo: pageViewController.view.topAnchor),
+            view.leadingAnchor.constraint(equalTo: pageViewController.view.leadingAnchor),
+            view.trailingAnchor.constraint(equalTo: pageViewController.view.trailingAnchor),
+            view.bottomAnchor.constraint(equalTo: pageViewController.view.bottomAnchor),
+        ])
+
+        pageViewController.setViewControllers([pages.first!], direction: .forward, animated: true, completion: nil)
+        currentVC = pages.first!
+        
+        // Close Button
+        closeButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        var closeConfig = UIButton.Configuration.borderless()
+        closeConfig.imagePadding = 8
+        closeConfig.title = "Close"
+        closeConfig.buttonSize = .small
+        
+        closeButton.configuration = closeConfig
+        closeButton.addTarget(self, action: #selector(closeTapped), for: .primaryActionTriggered)
+        
+        NSLayoutConstraint.activate([
+            //            closeButton.topAnchor.constraint(equalToSystemSpacingBelow: view.topAnchor, multiplier: 4),
+            closeButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 45),
+            closeButton.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            //            closeButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            //            closeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            //            view.trailingAnchor.constraint(equalToSystemSpacingAfter: closeButton.trailingAnchor, multiplier: 1)
+        ])
+        
+//        style()
+//        layout()
+        
+        super.viewDidLoad()
+    }
+    
+}
+
+extension OnBoardingContainerViewController {
+    private func style() {
+        
+        view.backgroundColor = .systemPurple
+        
+//        addChild(pageViewController)
+//        view.addSubview(pageViewController.view)
+        
+//        pageViewController.didMove(toParent: self)
+//        pageViewController.dataSource = self
+        
+//        pageViewController.view.translatesAutoresizingMaskIntoConstraints = false
+//
+//        NSLayoutConstraint.activate([
+//            view.topAnchor.constraint(equalTo: pageViewController.view.topAnchor),
+//            view.leadingAnchor.constraint(equalTo: pageViewController.view.leadingAnchor),
+//            view.trailingAnchor.constraint(equalTo: pageViewController.view.trailingAnchor),
+//            view.bottomAnchor.constraint(equalTo: pageViewController.view.bottomAnchor),
+//        ])
+        
+        // Close Button
+        closeButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        var closeConfig = UIButton.Configuration.borderless()
+        closeConfig.imagePadding = 8
+        closeConfig.title = "Close"
+        closeConfig.buttonSize = .small
+        
+        closeButton.configuration = closeConfig
+        closeButton.addTarget(self, action: #selector(closeTapped), for: .primaryActionTriggered)
+    }
+    
+    private func layout() {
+        view.addSubview(closeButton)
+        addChild(pageViewController)
+        view.addSubview(pageViewController.view)
+        
+        pageViewController.didMove(toParent: self)
+        pageViewController.dataSource = self
+        
         pageViewController.view.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
@@ -55,13 +138,26 @@ class OnBoardingContainerViewController:UIViewController {
             view.bottomAnchor.constraint(equalTo: pageViewController.view.bottomAnchor),
         ])
         
-        pageViewController.setViewControllers([pages.first!], direction: .forward, animated: true, completion: nil)
-        currentVC = pages.first!
         
-        super.viewDidLoad()
+        NSLayoutConstraint.activate([
+            //            closeButton.topAnchor.constraint(equalToSystemSpacingBelow: view.topAnchor, multiplier: 4),
+            closeButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 45),
+            closeButton.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            //            closeButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            //            closeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            //            view.trailingAnchor.constraint(equalToSystemSpacingAfter: closeButton.trailingAnchor, multiplier: 1)
+        ])
     }
-    
 }
+
+extension OnBoardingContainerViewController {
+    
+    @objc
+    private func closeTapped() {
+        print("Close was tapped!")
+    }
+}
+
 
 extension OnBoardingContainerViewController:UIPageViewControllerDataSource {
     
@@ -94,24 +190,3 @@ extension OnBoardingContainerViewController:UIPageViewControllerDataSource {
     }
 }
 
-// MARK: - ViewControllers
-class ViewController1: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemRed
-    }
-}
-
-class ViewController2: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemGreen
-    }
-}
-
-class ViewController3: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemBlue
-    }
-}
--- bankey-app/bankey-app/Onboarding/OnboardingViewController.swift
+++ bankey-app/bankey-app/Onboarding/OnboardingViewController.swift
@@ -9,17 +9,32 @@ import UIKit
 
 class OnboardingViewController:UIViewController {
     
-    let closeButton = UIButton()
+    
+    
     let stackView = UIStackView()
     let imageView = UIImageView()
     let label = UILabel()
+    
     let nextButton = UIButton()
     
+    let heroImageName:String
+    let titleText:String
     
     override func viewDidLoad() {
         super.viewDidLoad()
         style()
         layout()
+    }
+    
+    init(heroImageName:String, titleText:String) {
+        self.heroImageName = heroImageName
+        self.titleText = titleText
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
 }
@@ -30,6 +45,16 @@ extension OnboardingViewController {
         // View
         view.backgroundColor = .systemBackground
         
+//        // Close Button
+//        closeButton.translatesAutoresizingMaskIntoConstraints = false
+//        var closeConfig = UIButton.Configuration.borderless()
+//        closeConfig.imagePadding = 8
+//        closeConfig.title = "Close"
+//        closeConfig.buttonSize = .small
+//    
+//        closeButton.configuration = closeConfig
+//        closeButton.addTarget(self, action: #selector(closeTapped), for: .primaryActionTriggered)
+        
         // Stackview
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
@@ -38,28 +63,47 @@ extension OnboardingViewController {
         // Image
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
-        imageView.image = UIImage(named: "delorean")
+        imageView.image = UIImage(named: heroImageName)
         
         // Label
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "Bankey is faster, easier to use and has a brand new look and feel that will make you feel like you are back in the 1980s."
+        
+        label.text = titleText
         label.font = UIFont.preferredFont(forTextStyle: .title3)
         label.adjustsFontForContentSizeCategory = true
         label.numberOfLines = 0
         label.textAlignment = .center
         
+        // Next Button
+        nextButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        var nextConfig = UIButton.Configuration.borderless()
+        nextConfig.imagePadding = 8
+        nextConfig.title = "Next"
+        nextConfig.buttonSize = .medium
+        
+        nextButton.configuration = nextConfig
+        nextButton.addTarget(self, action: #selector(nextTapped), for: .primaryActionTriggered)
+
     }
     
     private func layout() {
         stackView.addArrangedSubview(imageView)
         stackView.addArrangedSubview(label)
         
+//        view.addSubview(closeButton)
         view.addSubview(stackView)
+        view.addSubview(nextButton)
         
-        NSLayoutConstraint.activate([
-            
-        ])
-        
+//        NSLayoutConstraint.activate([
+////            closeButton.topAnchor.constraint(equalToSystemSpacingBelow: view.topAnchor, multiplier: 4),
+//            closeButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 45),
+//            closeButton.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+////            closeButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+////            closeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+////            view.trailingAnchor.constraint(equalToSystemSpacingAfter: closeButton.trailingAnchor, multiplier: 1)
+//        ])
+
         NSLayoutConstraint.activate([
             stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
@@ -67,5 +111,22 @@ extension OnboardingViewController {
             view.trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 1)
         ])
         
+        NSLayoutConstraint.activate([
+            nextButton.bottomAnchor.constraint(equalToSystemSpacingBelow: view.bottomAnchor, multiplier: -2),
+            nextButton.trailingAnchor.constraint(equalToSystemSpacingAfter: view.trailingAnchor, multiplier: -3)
+        ])
+        
     }
+}
+
+extension OnboardingViewController {
+    @objc
+    private func nextTapped() {
+        print("Next was tapped")
+    }
+    
+//    @objc
+//    private func closeTapped() {
+//        print("Close was tapped!")
+//    }
 }
